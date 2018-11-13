79c64146a33702ae5177be927bd1641b84d02943
avidkucing <avidkucing@gmail.com>
2018-11-13 10:06:49 +0700

    initialized node and directory

---
+++ db/db.js
@@ -0,0 +1,15 @@
+const user = [
+    {
+        id : 'avid',
+        email: 'avidkucing@gmail.com',
+    },
+]
+
+const hub = [
+    {
+        id: 1,
+        name: 'example-hub',
+        userId: 'avid',
+        fields: ['name', 'email']
+    },
+]

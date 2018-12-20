f9444f7c8a18e2da8aa0ab2819bd0512d2d8df3b
avidkucing <avidkucing@gmail.com>
2018-12-20 10:29:34 +0700

    reuse pug

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express'),
+    app = express(),
+    passport = require('passport'),
+    auth = require('./auth');
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session');
+    port = 3000;
+
+app.set('view engine', 'pug');
+auth(passport);
+
+app.use(passport.initialize());
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['666']
+}));
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    if (req.session.token) {
+        res.cookie('token', req.session.token);
+        //res.json({
+          //  status: 'session cookie set'
+        //});
+        res.render('logout');
+    } else {
+        res.cookie('token', '');
+        //res.json({
+          //  status: 'session cookie not set'
+        //});
+        res.render('login');
+    }
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failed'
+    }),
+    (req, res) => {
+        req.session.token = req.user.token;
+        res.redirect('/');
+    }
+);
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session = null;
+    res.redirect('/');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+

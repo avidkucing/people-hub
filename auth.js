0913be60a9df821f9353c27e9a8e33182c502b4d
avidkucing <avidkucing@gmail.com>
2018-12-21 21:01:08 +0700

    change port

--- auth.js
+++ auth.js
@@ -9,7 +9,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
             clientID: '911561306189-q77psa6ng88e4vr4n965cjo0j5i8nulf.apps.googleusercontent.com',
             clientSecret: 'jFWFCqBtXZOYP5WSzkX9wE0T',
-            callbackURL: 'http://localhost:3000/auth/google/callback'
+            callbackURL: 'http://localhost:8080/auth/google/callback'
         },
         (token, refreshToken, profile, done) => {
             return done(null, {
